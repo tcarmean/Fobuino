4128df97f7c8da13f608479e372f4e9a8aefce92
Tim Carmean <tecarmea@mtu.edu>
2016-12-09 21:35:47 -0500

    add comments to the code

--- fobuino.ino
+++ fobuino.ino
@@ -1,12 +1,12 @@
-int button = 5;
-int presses = 2;
+int button = 5; //we are controlling the button via pin 5 on the Arduino UNO board
+int presses = 2; //simple control variable to keep track of how many times we "press" the button
 int wait = 750; //wait this many milliseconds. reset to 250 when done testing with DMM.
 
 void pressButton() {
-  digitalWrite(button, HIGH);
-  delay(wait);
-  digitalWrite(button, LOW);
-  delay(wait);
+  digitalWrite(button, HIGH); //ratchet the voltage to 5V
+  delay(wait); //sleep 750ms
+  digitalWrite(button, LOW); //cut the voltage to 0V (really 0.5 or something)
+  delay(wait); //sleep750ms
 }
 
 void setup() {
