b3c2e86cef1998ce2f4a742094541b1ca4fbdbf0
Tim Carmean <tecarmea@mtu.edu>
2016-12-09 20:09:23 -0500

    now stepping to 3.3v

--- fobuino.ino
+++ fobuino.ino
@@ -1,10 +1,10 @@
 int button = 5;
 int presses = 2;
-int volts = 153; //a value between 0 and 255 where 0 is 0 volts and 255 is 5 volts
+int volts = 165; //a value between 0 and 255 where 0 is 0 volts and 255 is 5 volts
 int wait = 5000; //wait this many milliseconds. reset to 250 when done testing with DMM.
 
 void pressButton() {
-  analogWrite(button, HIGH);
+  analogWrite(button, volts);
   delay(wait);
   analogWrite(button, LOW);
   delay(wait);
