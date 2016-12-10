a06873392d764e260082417b4c3deac106d19790
Tim Carmean <tecarmea@mtu.edu>
2016-12-10 10:40:14 -0500

    Implement hour long timer
    Use [Timer](https://github.com/JChristensen/Timer) Library to call the (newly defined) startCar callback every hour.
    
    The startCar callback replaces the old pressButton function. This callback handles pressing the button twice meaning that we only need to call the timer update function in the event loop.

--- fobuino.ino
+++ fobuino.ino
@@ -1,24 +1,30 @@
+#include <Event.h>
+#include <Timer.h>
+
+Timer t;
 int button = 5; //we are controlling the button via pin 5 on the Arduino UNO board
 int presses = 2; //simple control variable to keep track of how many times we "press" the button
-int wait = 750; //wait this many milliseconds. reset to 250 when done testing with DMM.
-int timer = 300000; //900000ms = 15 minutes
+int wait = 750; //simulate holding the button down for this many milliseconds to simulate a press and hold
+unsigned long int period = 60000; //60000ms = 1min
 
-void pressButton() {
-  digitalWrite(button, HIGH); //ratchet the voltage to 5V
-  delay(wait); //sleep 750ms
-  digitalWrite(button, LOW); //cut the voltage to 0V (really 0.5 or something)
-  delay(wait); //sleep750ms
+void startCar(void *context) {
+  //press the button twice to start the car
+  for (int i=presses; i>0; i--) {
+    digitalWrite(button, HIGH); //ratchet the voltage to 5V
+    delay(wait); //sleep 750ms
+    digitalWrite(button, LOW); //cut the voltage to 0V (really 0.5 or something)
+    delay(wait); //sleep750ms
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(button, OUTPUT);
+  digitalWrite(button, LOW);
+  t.every(period * 60, startCar, (void *)0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(timer);
-  for (int i=presses; i>0; i--) {
-    pressButton();
-  }
+  t.update();
 }
